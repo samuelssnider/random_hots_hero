7db9e5ef8922fd5871e7b1452925189169d032a9
samuelssnider <sam_redins@yahoo.com>
2018-09-16 12:10:27 -0600

    kinda lame so far but whatever

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -7,4 +7,49 @@ class Hero < ApplicationRecord
     top = (self.count + 1)
     Hero.find(Random.rand(top));
   end
+  
+  def self.tank
+    list = Hero.where(h_class: "Warrior")
+    list.shuffle[0]
+  end
+  
+  def self.spec
+    list = Hero.where(h_class: "Specialist")
+    list.shuffle[0]
+  end
+  
+  def self.sup
+    list = Hero.where(h_class: "Support")
+    list.shuffle[0]
+  end
+  
+  def self.assn
+    list = Hero.where(h_class: "Assassin")
+    list.shuffle[0]
+  end
+  
+  def self.wc
+    list = Hero.where(h_game: "Warcraft")
+    list.shuffle[0]
+  end
+  
+  def self.sc
+    list = Hero.where(h_game: "StarCraft")
+    list.shuffle[0]
+  end
+  
+  def self.dibs
+    list = Hero.where(h_game: "Diablo")
+    list.shuffle[0]
+  end
+  
+  def self.ow
+    list = Hero.where(h_game: "Overwatch")
+    list.shuffle[0]
+  end
+  
+  def self.blizz
+    list = Hero.where(h_game: "Blizzard")
+    list.shuffle[0]
+  end
 end
