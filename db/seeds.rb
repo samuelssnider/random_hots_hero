e4a5a8cec615fc3c1d00393059018f943e4b8a26
samuelssnider <sam_redins@yahoo.com>
2018-09-16 10:46:02 -0600

    save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ heroes = CSV.foreach "./db/data/characters.csv", headers: true, header_converter
 
 heroes.each do |hero|
   hero = hero.to_h
-  puts hero[:heroTitle]
-  Event.create!(title:      hero[:name],
+  puts hero[:name]
+  Hero.create!(title:      hero[:name],
                 class:      hero[:class],
                 game:       hero[:hero],
                 created_at: hero[:created_at]
